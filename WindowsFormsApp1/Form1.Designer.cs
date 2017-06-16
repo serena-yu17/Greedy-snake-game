e4b9f8f0fc0307822b6debf89bb1e342077872f0
Serena <sylvannus@gmail.com>
2017-06-17 04:11:30 +0930

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(899, 409);
+            this.trackBar1.Maximum = 4;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(208, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.trackBar1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(971, 381);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1035, 440);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(992, 440);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(949, 440);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(905, 440);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 25);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1079, 440);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "5";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(910, 534);
+            this.checkBox1.MaximumSize = new System.Drawing.Size(200, 200);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(185, 77);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Allow crossing outer borders";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.checkBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(134, 297);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(567, 129);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Press any key to begin";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(969, 239);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 25);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Width";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(897, 278);
+            this.trackBar2.Maximum = 3;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(208, 56);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Value = 3;
+            this.trackBar2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.trackBar2.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(903, 309);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 25);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(962, 309);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 25);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(1018, 309);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(23, 25);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "3";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(1077, 309);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(23, 25);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "4";
+            // 
+            // label14
+            // 
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(862, 82);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(283, 112);
+            this.label14.TabIndex = 8;
+            this.label14.Text = "Use W,A,S,D or arrow keys to control, Esc to reset. ";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label14.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Size = new System.Drawing.Size(830, 730);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("HelveticaInseratCyr Upright", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label13.Location = new System.Drawing.Point(226, 321);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(393, 96);
+            this.label13.TabIndex = 7;
+            this.label13.Text = "Game Over";
+            this.label13.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 772);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "The Greedy Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        sectList snake = new sectList();
+        Boolean gameHandler = false;
+        sect apple = new sect();
+        const int SIZE = 599;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Point gbP = new Point();
+            gbP.X = 12;
+            gbP.Y = 12;
+            groupBox1.Location = gbP;
+            groupBox1.Height = SIZE + 1;
+            groupBox1.Width = SIZE + 1;
+            label13.Hide();
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            if (incre.x == incRec.x && incre.y == -(incRec.y) && incre.y != 0)
+            {
+                incre.y = incRec.y;
+            }
+            else if (incre.x == -(incRec.x) && incre.y == incRec.y && incre.x != 0)
+            {
+                incre.x = incRec.x;
+            }
+            incRec.x = incre.x;
+            incRec.y = incre.y;
+            Point addPoint = new Point();
+            if (checkBox1.Checked)
+                turnAround(ref addPoint);
+            else
+            {
+                addPoint.X = snake.end.point.X + incre.x;
+                addPoint.Y = snake.end.point.Y + incre.y;
+                if (addPoint.X > SIZE - snakeWidth.width || addPoint.Y > SIZE - snakeWidth.width)
+                {
+                    gameOver();
+                    return;
+                }
+            }
+            if (snake.overlaps(addPoint))
+            {
+                gameOver();
+                return;
+            }
+            if (sectList.collis(apple.point, addPoint))
+            {
+                snake.push_b(addPoint, groupBox1);
+                setApple();
+            }
+            else
+            {
+                snake.push_b(addPoint, groupBox1);
+                snake.pop_f();
+            }
+        }
+
+        void turnAround(ref Point addPoint)
+        {
+            int unit = snakeWidth.width;
+            if (snake.end.point.X <= unit && incre.x < 0)
+                addPoint.X = SIZE;
+            else if (snake.end.point.X >= SIZE - unit && incre.x > 0)
+                addPoint.X = 0;
+            else
+                addPoint.X = snake.end.point.X + incre.x;
+
+            if (snake.end.point.Y <= unit && incre.y < 0)
+                addPoint.Y = SIZE;
+            else if (snake.end.point.Y >= SIZE - unit && incre.y > 0)
+                addPoint.Y = 0;
+            else
+                addPoint.Y = snake.end.point.Y + incre.y;
+        }
+
+
+        void initGame()
+        {
+            gameHandler = true;
+            snake.clear();
+            int speed = trackBar1.Value + 1;
+            int interval = (int)(500 * Math.Pow(0.6, speed));
+            timer1.Interval = interval;
+            snakeWidth.width = trackBar2.Value * 2 + 5;
+            Point startPoint = new Point();
+            int unit = snakeWidth.width * 2 + 1;
+            int unitCount = SIZE / unit - 1;
+            unitCount /= 2;
+            startPoint.X = unitCount * unit + snakeWidth.width;
+            startPoint.Y = unitCount * unit + snakeWidth.width;
+            snake.push_b(startPoint, groupBox1);
+            incre.x = (snakeWidth.width * 2 + 1);
+            incre.y = 0;
+            incRec.x = incre.x;
+            incRec.y = incre.y;
+            startPoint.X += incre.x;
+            startPoint.Y += incre.y;
+            snake.push_b(startPoint, groupBox1);
+            label7.Hide();
+            this.Update();
+            timer1.Start();
+            setApple();
+            trackBar1.Enabled = false;
+            trackBar2.Enabled = false;
+            checkBox1.Enabled = false;
+        }
+
+        void gameOver()
+        {
+            this.Update();
+            timer1.Stop();
+            for (int i = 0; i < 3; i++)
+            {
+                label13.Show();
+                this.Update();
+                Thread.Sleep(1000);
+                label13.Hide();
+                this.Update();
+                Thread.Sleep(500);
+            }
+            trackBar1.Enabled = true;
+            trackBar2.Enabled = true;
+            checkBox1.Enabled = true;
+            apple.picBox.Dispose();
+            snake.clear();
+            gameHandler = false;
+            label7.Show();
+            this.Update();
+        }
+
+        void setApple()
+        {
+            if (apple.picBox != null)
+                apple.picBox.Dispose();
+            Random random = new Random();
+            int unit = snakeWidth.width * 2 + 1;
+            int unitCount = SIZE / unit - 1;
+            int x = random.Next(0, unitCount) * unit + snakeWidth.width;
+            int y = random.Next(0, unitCount) * unit + snakeWidth.width;
+            Point applePoint = new Point(x, y);
+            while (snake.overlaps(applePoint))
+            {
+                x = random.Next(0, unitCount) * unit + snakeWidth.width;
+                y = random.Next(0, unitCount) * unit + snakeWidth.width;
+                applePoint.X = x;
+                applePoint.Y = y;
+            }
+            apple.point = applePoint;
+            apple.draw(Color.Red, groupBox1);
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true;
+            if (gameHandler == false)
+            {
+                initGame();
+                return;
+            }
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                if (incre.y > 0)
+                    return;
+                incre.x = 0;
+                incre.y = -1 * (snakeWidth.width * 2 + 1);
+            }
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (incre.y < 0)
+                    return;
+                incre.x = 0;
+                incre.y = 1 * (snakeWidth.width * 2 + 1);
+            }
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                if (incre.x > 0)
+                    return;
+                incre.x = -1 * (snakeWidth.width * 2 + 1);
+                incre.y = 0;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                if (incre.x < 0)
+                    return;
+                incre.x = 1 * (snakeWidth.width * 2 + 1);
+                incre.y = 0;
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                label7.Text = "Press any key to start";
+                label7.Show();
+                timer1.Stop();
+                snake.clear();
+                apple.picBox.Dispose();
+                gameHandler = false;
+                trackBar1.Enabled = true;
+                trackBar2.Enabled = true;
+                checkBox1.Enabled = true;
+                this.Update();
+            }
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Left:
+                case Keys.Right:
+                    e.IsInputKey = true;
+                    break;
+            }
+        }
+    }
+
+    public static class snakeWidth
+    {
+        public static int width = 0;
+    }
+
+    public static class incre
+    {
+        public static int x;
+        public static int y;
+    }
+
+    public static class incRec
+    {
+        public static int x;
+        public static int y;
+    }
+
+    public class sect
+    {
+        public sect next;
+        public Point point;
+        public PictureBox picBox;
+        public void draw(Color color, GroupBox gb)
+        {
+            Point topLeft = new Point();
+            topLeft.X = point.X - snakeWidth.width;
+            topLeft.Y = point.Y - snakeWidth.width;
+            picBox = new PictureBox();
+            picBox.Location = topLeft;
+            picBox.Width = (snakeWidth.width * 2 + 1);
+            picBox.Height = (snakeWidth.width * 2 + 1);
+            picBox.BackColor = color;
+            gb.Controls.Add(picBox);
+        }
+    }
+
+    public class sectList
+    {
+        public sect head, end;
+
+        public void push_b(Point newCoord, GroupBox gb)
+        {
+            if (head == null)
+            {
+                head = new sect();
+                head.point = newCoord;
+                head.next = null;
+                end = head;
+            }
+            else
+            {
+                sect newSect = new sect();
+                newSect.point = newCoord;
+                end.next = newSect;
+                end = end.next;
+            }
+            end.draw(Color.DarkGray, gb);
+        }
+
+        public void pop_f()
+        {
+            head.picBox.Dispose();
+            sect temp = head.next;
+            head = temp;
+        }
+
+        public Boolean overlaps(Point pScanned)
+        {
+            if (head == null)
+                return false;
+            sect iterate = head;
+            while (iterate != end)
+            {
+                if (collis(iterate.point, pScanned))
+                    return true;
+                iterate = iterate.next;
+            }
+            if (collis(iterate.point, pScanned))
+                return true;
+            return false;
+        }
+
+        public void clear()
+        {
+            if (head == null)
+                return;
+            sect i = head;
+            while (i != end)
+            {
+                i.picBox.Dispose();
+                i = i.next;
+            }
+            i.picBox.Dispose();
+            head = null;
+            end = null;
+        }
+
+        //public Boolean secondLast(Point p)
+        //{
+        //    if (head == null || head == end)
+        //        return false;
+        //    sect iterate = head;
+        //    while (iterate.next != end)
+        //        iterate = iterate.next;
+        //    if (collis(iterate.point, p))
+        //        return true;
+        //    return false;
+        //}
+
+        public static Boolean collis(Point a, Point b)
+        {
+            if (Math.Abs(a.X - b.X) < (snakeWidth.width * 2 + 1) && Math.Abs(a.Y - b.Y) < (snakeWidth.width * 2 + 1))
+                return true;
+            else
+                return false;
+        }
+    }
+}
